433040922c44d71c0d62d0be6cdcaa1c3af4830f
azhar-mohammed <mohd.azhar.moinuddin@gmail.com>
2018-02-05 22:41:32 +0530

    Atomic integer addded to keep track of generated csv files.

--- CsvSplitter/src/main/java/com/csvsplitter/CsvSplitterRecursiveAction.java
+++ CsvSplitter/src/main/java/com/csvsplitter/CsvSplitterRecursiveAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 
@@ -18,6 +19,7 @@ public class CsvSplitterRecursiveAction extends RecursiveAction {
 	private String outputDirectory;
 	private List<String> linesList;
 	private int noOfLines;
+	private static final AtomicInteger atomicInt = new AtomicInteger();
 
 	public CsvSplitterRecursiveAction(final String outputDirectory, final List<String> fileLinesList) {
 		super();
@@ -25,7 +27,11 @@ public class CsvSplitterRecursiveAction extends RecursiveAction {
 		this.linesList = fileLinesList;
 		this.noOfLines = linesList.size();
 	}
-
+/**
+ * The main recursive computation performed by this task. Here if the no of lines is less than
+ * threshold than we write the content to a file ,else we go ahead with a divide and conquer approach and 
+ * we try to recursively call this method.
+ */
 	@Override
 	protected void compute() {
 		
@@ -35,16 +41,19 @@ public class CsvSplitterRecursiveAction extends RecursiveAction {
 		}
 		else
 		{
-			RecursiveAction first = new CsvSplitterRecursiveAction(outputDirectory,linesList.subList(0,linesList.size()/2));
-			RecursiveAction second = new CsvSplitterRecursiveAction(outputDirectory,linesList.subList(linesList.size()/2,linesList.size())); 
+			RecursiveAction first = new CsvSplitterRecursiveAction(outputDirectory,linesList.subList(0,threshold));
+			RecursiveAction second = new CsvSplitterRecursiveAction(outputDirectory,linesList.subList(threshold,linesList.size())); 
 			invokeAll(first,second);
 		}
 
 	}
-
+/**
+ *  @param lines
+ *  This method writes the provided input lines to an output csv file and increments the atomic integer value
+ */
 	private void writeToFile(List<String> lines) {
 
-		try (FileWriter fw = new FileWriter(outputDirectory + ThreadLocalRandom.current().nextLong() + ".txt")) {
+		try (FileWriter fw = new FileWriter(outputDirectory + atomicInt.getAndIncrement() + ".txt")) {
 			for (String s : lines) {
 				fw.write(s);
 				fw.write("\n");
